2cf4c3a49a17bc8045342e84ba9e92e2ff388684
STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>
2024-12-04 13:06:50 +0000

    Signed-off-by: STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>

---
+++ Project 2/Assets/VictimScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimScript : MonoBehaviour
+{
+    internal void Bump(float explosionForce, Vector3 explosionPosition, float explosionRadius, float explosionStrength)
+    {
+        print("Bumped !!");
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.red;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null )
+        {
+            rb.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
