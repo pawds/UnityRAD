5d4d7122163828b1c65813fe876266937bd36e37
STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>
2024-12-11 11:32:23 +0000

    Signed-off-by: STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>

--- Project 2/Assets/ExplosionScript.cs
+++ Project 2/Assets/ExplosionScript.cs
@@ -4,10 +4,24 @@ using System.Collections;
 public class ExplosionScript : MonoBehaviour
 {
     public float radius = 5.0F;
-    public float power = 300F;
+    public float power = 100F;
 
 
-    void Start()
+ 
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    private void OnCollisionEnter(Collision collision)
     {
         Vector3 explosionPos = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
@@ -16,18 +30,8 @@ public class ExplosionScript : MonoBehaviour
             Rigidbody rb = hit.GetComponent<Rigidbody>();
 
             if (rb != null)
-                rb.AddExplosionForce(power, explosionPos, radius, 300F);
-        }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+                rb.AddExplosionForce(power, explosionPos, radius, 100F);
         }
     }
+
 }
