159f1a863102a26db3449e11045c998bb297fc73
STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>
2024-12-12 10:04:15 +0000

    Signed-off-by: STUDENT Padraig Enright <Padraig.Enright@students.ittralee.ie>

--- Project 2/Assets/WinScript.cs
+++ Project 2/Assets/WinScript.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class WinScript : MonoBehaviour
 {
+
+    TextDisplay textDisplay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        textDisplay = FindObjectOfType<TextDisplay>();
+        textDisplay.turnOff();
+        if (textDisplay != null)
+            print("I found the text ");
+        else
+            print("No Text!!");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Return))
+            textDisplay.turnOn();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -20,7 +29,7 @@ public class WinScript : MonoBehaviour
         if (collision.collider.name == "Victim")
         {
             print("cat wrangled");
-            GetComponent<TextDisplay>();
+            textDisplay.turnOn();
 
         }
     }
